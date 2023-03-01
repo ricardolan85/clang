cbe62f55e103345798381a7e8bf2188969960d94
Ricardo Landim <ricardolan@gmail.com>
2023-03-01 17:05:34 -0300

    makefiles

--- pointer/pointer.c
+++ pointer/pointer.c
@@ -7,9 +7,16 @@ const int max = 100;
 
 int main(int argc, char **argv){
 
+    int local = 0x90;
+
+    printf("argc: %p\n",&argc);
+    printf("argv: %p\n",argv);
+    
     printf("global no-init: %p\n",&len);
     printf("global initialized: %p\n",&year);
     printf("global const: %p\n",&max);
+
+    printf("local: %p\n",&local);
 
     return 0;
     
